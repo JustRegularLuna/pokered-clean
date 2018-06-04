c9d53d75bf312e457fed5d4584fdaf27a185ea88
TheFakeMateo <skeetendomateo@gmail.com>
2018-06-04 16:27:47 -0500

    Remove pointless leftover Japanese grammar checks

--- text.asm
+++ text.asm
@@ -1297,11 +1297,7 @@ _MoveIsDisabledText::
 _MonName1Text::
 	text "<USER>@@"
 
-_Used1Text::
-	text ""
-	line "used @@"
-
-_Used2Text::
+_UsedText::
 	text ""
 	line "used @@"
 
@@ -1311,25 +1307,6 @@ _InsteadText::
 
 _CF4BText::
 	TX_RAM wcf4b
-	text "@"
-
-_ExclamationPoint1Text::
-	text "!"
-	done
-
-_ExclamationPoint2Text::
-	text "!"
-	done
-
-_ExclamationPoint3Text::
-	text "!"
-	done
-
-_ExclamationPoint4Text::
-	text "!"
-	done
-
-_ExclamationPoint5Text::
 	text "!"
 	done
 
